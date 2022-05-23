60e43bbf065c0e82c81a624a6f79e4bd8fb6b317
Aurel1701 <gantzfan96@gmail.com>
2022-05-23 14:12:36 +0200

    edit finder

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/* Riscrivere questa pagina del sito google https://policies.google.com/faq.
+Ci sono diverse domande con relative risposte.
+Gestire il “Database” e la visualizzazione di queste domande e risposte con PHP. */
+
+/* creo il contento da stampare a schemro */
+
+$faq = [
+
+    
+]
+
+?>
